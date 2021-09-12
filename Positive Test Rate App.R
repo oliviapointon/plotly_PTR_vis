d8bbbef1322e03b6051a62044840b7efd0041e52
oliviapointon <61777875+oliviapointon@users.noreply.github.com>
2021-09-12 21:12:09 +1000

    Update Positive Test Rate App.R

--- Positive Test Rate App.R
+++ Positive Test Rate App.R
@@ -1,13 +1,13 @@
-# Set the working directory
-setwd("~/~ COVID Analysis ~")
-
-# Call relevant packages
+# --- Call relevant packages ---
 library(plotly)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(reshape2)
+library(withr)
 library(htmlwidgets)
+
+# --- Data reading and preparation ---
 
 # Read in the data
 Pos_Test_Data <- read.csv("Positive Test Rate by State.csv", header=TRUE, fileEncoding = "UTF-8-BOM")
@@ -15,63 +15,67 @@ Pos_Test_Data <- read.csv("Positive Test Rate by State.csv", header=TRUE, fileEn
 # Replace blank values of moving average with NA
 Pos_Test_Data$X5.Day.Moving.Average <- ifelse(Pos_Test_Data$X5.Day.Moving.Average=="", NA, Pos_Test_Data$X5.Day.Moving.Average)
 
-# Replace Date with a date variable
-Pos_Test_Data$Date <- dmy(Pos_Test_Data$Date)
+# Convert date to date variable using lubridate package
+Pos_Test_Data$Date <- Pos_Test_Data$Date %>%
+  dmy()
 
-# Convert the data from wide to narrow, to be able to populate the legend
+# Convert the data from wide to narrow. This format ensures the 'variable' column can be used to define the different plot series on the legend
 Pos_Test_Data_Narrow <- melt(Pos_Test_Data, id.vars=c("State", "Date"))
 
 # --- Calculating axis ranges for each state ---
 
-ACT_Data <- Pos_Test_Data_Narrow %>%
-    subset(State=="ACT")
+# Calculating transformation coefficients for ACT
 
-NSW_Data <- Pos_Test_Data_Narrow %>%
-  subset(State=="NSW")
-
-
-#Calculating transformation coefficients for ACT
-#Apply a linear transformation to convert both y axes
-#Using the formula b = (y2-y1/x2-x1) and a = y1 - (b*x1)
-#But we set x1 and y1 to 0 since that's where we want both our vertical axes to start
-y2_ACT <- ACT_Data %>%
-  subset(variable=="Positive.Test.Rate") %>%
+# Apply a linear transformation to convert both y axes
+# Using the formula y = a + bx, where y and x are vectors and a and b are scalars
+# i.e. b = (y2-y1/x2-x1) and a = y1 - (b*x1)
+# But we set x1 and y1 to 0 since that's where we want both our vertical axes to start
+y2_ACT <- Pos_Test_Data_Narrow %>%
+  subset(variable=="Positive.Test.Rate" & State=="ACT") %>%
   select(value) %>%
   max()
-x2_ACT <- ACT_Data %>%
-  subset(variable=="Tests") %>%
+x2_ACT <- Pos_Test_Data_Narrow %>%
+  subset(variable=="Tests" & State=="ACT") %>%
   select(value) %>%
   max()
 b_ACT <- y2_ACT / x2_ACT
 
-#Also calculate min and max of date axis
-mindate_ACT <- ACT_Data$Date %>%
-  min()
-maxdate_ACT <- ACT_Data$Date %>%
-  max()
-mindate_NSW <- NSW_Data$Date %>%
-  min()
-maxdate_NSW <- NSW_Data$Date %>%
-  max()
-
-#Calculating transformation coefficients for NSW
-#y1_NSW <- NSW_Data %>%
-#  subset(variable=="Positive.Test.Rate") %>%
-#  select(value) %>%
-#  min()
-y2_NSW <- NSW_Data %>%
-  subset(variable=="Positive.Test.Rate") %>%
+# Calculating transformation coefficients for NSW
+y2_NSW <- Pos_Test_Data_Narrow %>%
+  subset(variable=="Positive.Test.Rate" & State=="NSW") %>%
   select(value) %>%
   max()
-x2_NSW <- NSW_Data %>%
-  subset(variable=="Tests") %>%
+x2_NSW <- Pos_Test_Data_Narrow %>%
+  subset(variable=="Tests" & State=="NSW") %>%
   select(value) %>%
   max()
 b_NSW <- y2_NSW / x2_NSW
-#b_NSW <- (y2_NSW - y1_NSW) / (x2_NSW - x1_NSW)
-#a_NSW <- y1_NSW - b_NSW * x1_NSW
 
-#Adding scaled test data back to dataframe
+# Also calculate min and max of date axis
+# Write a function that takes the State and function (min or max) as inputs
+date.axis.bounds.calc <- function(StateName, fn){
+  if (fn=="max") {
+    (Pos_Test_Data_Narrow %>%
+       subset(State==StateName))$Date %>%
+      max()   
+  }
+  else if (fn=="min") {
+    (Pos_Test_Data_Narrow %>%
+       subset(State==StateName))$Date %>%
+      min()   
+  }
+  # If any function other than min or max has been entered, throw an error
+  else {
+    stop('Invalid function name entered.')
+  }
+}
+
+mindate_ACT <- date.axis.bounds.calc("ACT", "min")
+maxdate_ACT <- date.axis.bounds.calc("ACT", "max")
+mindate_NSW <- date.axis.bounds.calc("NSW", "min")
+maxdate_NSW <- date.axis.bounds.calc("NSW", "max")
+
+# Apply the linear transformations to the "Tests" column, and append this data to the dataframe as "Tests.Scaled"
 Pos_Test_Data_Narrow <- rbind(Pos_Test_Data_Narrow,
                               Pos_Test_Data_Narrow %>%
     subset(variable=="Tests" & State=="ACT") %>%
@@ -83,8 +87,10 @@ Pos_Test_Data_Narrow <- rbind(Pos_Test_Data_Narrow,
                                 subset(variable=="Tests" & State=="NSW") %>%
                                 mutate(variable="Tests.Scaled") %>%
                                 mutate(value=value* b_NSW))
-                                #mutate(value=value* b_NSW + a_NSW))
 
+# --- Creating initial ggplot ---
+
+# Note that all 6 data series are added to the same plot
 scatter_plot <- ggplot(Pos_Test_Data_Narrow, aes(x=Date, y=value)) +
   ylab("Positive Test Rate") +
   scale_y_continuous(labels=scales::percent, sec.axis = sec_axis(~./ b_ACT, name = "Number of Tests Administered")) +
@@ -112,34 +118,39 @@ scatter_plot <- ggplot(Pos_Test_Data_Narrow, aes(x=Date, y=value)) +
   theme(legend.position = "bottom")
 plot(scatter_plot)
 
-#Interactive version
+# --- Parsing ggplot output to ggplotly to create an interactive version of the scatterplot ---
+# Add a buffer when passing values to the range() function. This ensures the edges of the series aren't cut off.
 scatter_plot_interactive <- ggplotly(scatter_plot, tooltip="text") %>%
   add_lines(name="", opacity=0, x =~Date, y=~(value/b_ACT), yaxis="y2", visible=FALSE, showlegend=FALSE, inherit=FALSE, color=NULL) %>%
   layout (
-    yaxis2 = list(side = "right", title = list(text="Number of Tests Administered",font=list(size=14)) , linewidth = 0, overlaying = "y", showgrid=FALSE, range=c(0,x2_ACT+100), tickfont=list(size=14), showline=FALSE, tickformat=",.1r"),
+    yaxis2 = list(side = "right", title = list(text="Number of Tests Administered",font=list(size=14)) , linewidth = 0, overlaying = "y", showgrid=FALSE, range=c(0,x2_ACT+100),
+                  tickfont=list(size=14), showline=FALSE, tickformat=",.1r"),
     legend=list(x=1.15),
-    yaxis=list(range=c(0,y2_ACT+0.001),title=list(text="Positive Test Rate", font=list(size=14)),tickformat=".2%", tick0=0, dtick=0.002, tickmode="linear", tickfont=list(size=14)),
-    xaxis=list(tickmode="array", tickvals=list(18840, 18854, 18868, 18882), ticktext=list("Aug 01", "Aug 15", "Aug 29", "Sep 12"), tickfont=list(size=14), showline=TRUE, linecolor="#A9a9a9", tickcolor="#A9a9a9", showspikes=TRUE, ticks="outside", ticklen=5, tickwidth=1, title=list(text="Date", font=list(size=14)), showgrid=FALSE),
+    yaxis=list(range=c(0,y2_ACT+0.001),title=list(text="Positive Test Rate", font=list(size=14)),tickformat=".2%", tick0=0, dtick=0.002, tickmode="linear",
+               tickfont=list(size=14)),
+    xaxis=list(tickmode="array", tickvals=list(18840, 18854, 18868, 18882), ticktext=list("Aug 01", "Aug 15", "Aug 29", "Sep 12"), tickfont=list(size=14),
+               showline=TRUE, linecolor="#A9a9a9", tickcolor="#A9a9a9", showspikes=TRUE, ticks="outside", ticklen=5, tickwidth=1, title=list(text="Date", font=list(size=14)),
+               showgrid=FALSE, range=c(as.numeric(mindate_ACT)-2,as.numeric(maxdate_ACT)+2)),
     title=list(font=list(size=20), text="Positive Test Rate - ACT", x=0, xref='paper'),
     margin=list(l=80, r=80, t=50, b=50)
 )
-#range=c(mindate_ACT,maxdate_ACT),
 
-#Update legend values
-#List element 1: ACT tests
+# Update legend text
+# List element 1: ACT tests
 scatter_plot_interactive$x$data[[1]]$name <- "Number of Tests (RHS)"
-#List element 2: NSW tests
+# List element 2: NSW tests
 scatter_plot_interactive$x$data[[2]]$name <- "Number of Tests (RHS)"
-#List element 3: ACT positive test rate
+# List element 3: ACT positive test rate
 scatter_plot_interactive$x$data[[3]]$name <- "Daily Positive Test Rate (LHS)"
-#List element 4: NSW positive test rate
+# List element 4: NSW positive test rate
 scatter_plot_interactive$x$data[[4]]$name <- "Daily Positive Test Rate (LHS)"
-#List element 5: ACT 5 day moving average
+# List element 5: ACT 5 day moving average
 scatter_plot_interactive$x$data[[5]]$name <- "5 Day Moving Average <br> Positive Test Rate (LHS)"
-#List element 6: NSW 5 day moving average
+# List element 6: NSW 5 day moving average
 scatter_plot_interactive$x$data[[6]]$name <- "5 Day Moving Average <br> Positive Test Rate (LHS)"
 
-#Initiate plot to ACT
+# We only want one state's data to show at a time. Initiate the plot to the ACT. 
+# That is, the series associated with ACT will be initially visible, and the series associated with other states will be initially invisible.
 scatter_plot_interactive$x$data[[1]]$visible <- TRUE
 scatter_plot_interactive$x$data[[2]]$visible <- FALSE
 scatter_plot_interactive$x$data[[3]]$visible <- TRUE
@@ -153,42 +164,53 @@ scatter_plot_interactive$x$data[[4]]$showlegend <- FALSE
 scatter_plot_interactive$x$data[[5]]$showlegend <- TRUE
 scatter_plot_interactive$x$data[[6]]$showlegend <- FALSE
 
-#Add a buffer either side of x axis
-scatter_plot_interactive$x$layout$xaxis$range <- c(as.numeric(mindate_ACT)-2,as.numeric(maxdate_ACT)+2)
-
-#Remove 'alpha' annotation
+# Plotly gets confused by the 'alpha' aesthetic from ggplot2. The opacity displays correctly, but there is also an additional annotation added to the chart. Let's remove this
+# by setting to null.
 scatter_plot_interactive$x$layout$annotations[[1]] <- NULL
 
-#Drop down menu for different state's data
+# --- Adding a drop down menu to toggle the different state's data ---
 scatter_plot_interactive_menu <- scatter_plot_interactive %>% layout(
   updatemenus = list(
     list(
+      # This determines the x- and y-coordinates of where to place the dropdown
       x = 1.35,
       y = 0.5,
       buttons = list(
+        # List the actions the ACT button should perform
+        # This includes toggling the visible/invisible status of each series
+        # And rescaling the axes to fit the data snugly
         list(method = "update",
              args = list(list("visible"=list(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE),
                               "showlegend"=list(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE)),
-                         list("yaxis2"=list(side = "right", title = list(text="Number of Tests Administered",font=list(size=14)) , linewidth = 0, overlaying = "y", showgrid=FALSE,range=c(0,x2_ACT+100), tickfont=list(size=14), tickformat=",.1r"),
+                         list("yaxis2"=list(side = "right", title = list(text="Number of Tests Administered",font=list(size=14)) , linewidth = 0, overlaying = "y",
+                                            showgrid=FALSE,range=c(0,x2_ACT+100), tickfont=list(size=14), tickformat=",.1r"),
                               "yaxis"=list(range=c(0,y2_ACT+0.001),title=list(text="Positive Test Rate", font=list(size=14)),tickformat=".2%", tickfont=list(size=14)),
-                              "xaxis"=list(range=c(mindate_ACT,maxdate_ACT),tickmode="array", tickvals=list(18840, 18854, 18868, 18882), ticktext=list("Aug 01", "Aug 15", "Aug 29", "Sep 12"), linecolor="#A9a9a9", tickcolor="#A9a9a9", showspikes=TRUE, ticklen=5, title=list(text="Date", font=list(size=14)),tickfont=list(size=14)),
+                              "xaxis"=list(range=c(mindate_ACT,maxdate_ACT),tickmode="array", tickvals=list(18840, 18854, 18868, 18882),
+                                           ticktext=list("Aug 01", "Aug 15", "Aug 29", "Sep 12"), linecolor="#A9a9a9", tickcolor="#A9a9a9", showspikes=TRUE, ticklen=5,
+                                           title=list(text="Date", font=list(size=14)),tickfont=list(size=14)),
                               "title"=list(font=list(size=20), text="Positive Test Rate - ACT", x=0, xref='paper'),
                               "margin"=list(l=80, r=80, t=50, b=50))),
              label = "ACT"),
+        # Similarly, list the actions the NSW button should perform
         list(method = "update",
              args = list(list("visible"=list(FALSE, TRUE, FALSE, TRUE, FALSE, TRUE),
-                              "showlegend"=list(FALSE, TRUE,FALSE,TRUE,FALSE,TrRUE)),
-                         list("yaxis2"=list(side = "right", title = list(text="Number of Tests Administered",font=list(size=14)) , linewidth = 0, overlaying = "y", showgrid=FALSE,range=c(0,x2_NSW+100), tickfont=list(size=14), tickformat=",.2s"),
+                              "showlegend"=list(FALSE, TRUE,FALSE,TRUE,FALSE,TRUE)),
+                         list("yaxis2"=list(side = "right", title = list(text="Number of Tests Administered",font=list(size=14)), linewidth = 0, overlaying = "y",
+                                            showgrid=FALSE, range=c(0,x2_NSW+100), tickfont=list(size=14), tickformat=",.2s"),
                               "yaxis"=list(range=c(0,y2_NSW+0.001),title=list(text="Positive Test Rate", font=list(size=14)),tickformat=".2%", tickfont=list(size=14)),
-                              "xaxis"=list(range=c(mindate_NSW,maxdate_NSW),tickmode="array", tickvals=list(18840, 18854, 18868, 18882), ticktext=list("Aug 01", "Aug 15", "Aug 29", "Sep 12"), linecolor="#A9a9a9", tickcolor="#A9a9a9", showspikes=TRUE, ticklen=5, title=list(text="Date", font=list(size=14)),tickfont=list(size=14)),
+                              "xaxis"=list(range=c(mindate_NSW,maxdate_NSW),tickmode="array", tickvals=list(18840, 18854, 18868, 18882),
+                                           ticktext=list("Aug 01", "Aug 15", "Aug 29", "Sep 12"), linecolor="#A9a9a9", tickcolor="#A9a9a9", showspikes=TRUE, ticklen=5,
+                                           title=list(text="Date", font=list(size=14)),tickfont=list(size=14)),
                               "title"=list(font=list(size=20), text="Positive Test Rate - NSW", x=0, xref='paper'),
                               "margin"=list(l=80, r=80, t=50, b=50))),
              label = "NSW")))
      ),
+  # Add a label above the dropdown
   annotations=list(text="<b> Choose State: </b>", x=1.4, y=0.55, xref='paper', yref='paper', showarrow=FALSE)
 )
 
+# Print the output to the plotting window
 print(scatter_plot_interactive_menu)
 
-#Output a HTML file in the working directory
-withr::with_dir(getwd(), htmlwidgets::saveWidget(scatter_plot_interactive_menu, "plot.html"))
+# Output a HTML file in the working directory
+with_dir(getwd(), saveWidget(scatter_plot_interactive_menu, "plot.html"))
